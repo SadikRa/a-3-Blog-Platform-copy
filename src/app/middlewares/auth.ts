c1849a5d55b329d584149a7bc4c125d9bcedd7e1
SadikRa <sadikrahman494@gmail.com>
2025-01-30 13:04:11 +0600

    auth file updated

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -1,39 +1,35 @@
-import { NextFunction, Request, Response } from 'express';
-import { StatusCodes } from 'http-status-codes';
 import jwt, { JwtPayload } from 'jsonwebtoken';
-import config from '../config';
-import AppError from '../errors/AppError';
-import { TUserRole } from '../modules/user/user.interface';
-import { User } from '../modules/user/user.model';
+import { StatusCodes } from 'http-status-codes';
+import { NextFunction, Request, Response } from 'express';
 import catchAsync from '../utils/catchAsync';
+import AppError from '../errors/AppError';
+import config from '../config';
 
-const authorize = (...allowedRoles: TUserRole[]) => {
+const authorize = () => {
   return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-    const token = req.headers.authorization?.split(' ')[1]; 
+    const token = req.headers.authorization;
 
     if (!token) {
-      throw new AppError(StatusCodes.UNAUTHORIZED, 'Access denied. Token is missing.');
+      throw new AppError(StatusCodes.UNAUTHORIZED, 'you are not UNAUTHORIZED');
     }
 
-    const decoded = jwt.verify(token, config.jwt_access_secret as string) as JwtPayload;
+    jwt.verify(
+      token,
+      config.jwt_access_secret as string,
+      function (err, decoded) {
+        if (err) {
+          throw new AppError(
+            StatusCodes.UNAUTHORIZED,
+            'you are not UNAUTHORIZED',
+          );
+        }
 
-    const { userId, role } = decoded;
+        //decoded
+        req.user = decoded as JwtPayload;
 
-    const user = await User.findById(userId);
-    if (!user) {
-      throw new AppError(StatusCodes.NOT_FOUND, 'User not found in the system.');
-    }
-
-    if (user.isBlocked) {
-      throw new AppError(StatusCodes.FORBIDDEN, 'Your account has been blocked.');
-    }
-
-    if (allowedRoles.length && !allowedRoles.includes(role as TUserRole)) {
-      throw new AppError(StatusCodes.FORBIDDEN, 'You do not have permission to perform this action.');
-    }
-
-    req.user = { userId, role };
-    next();
+        next();
+      },
+    );
   });
 };
 
